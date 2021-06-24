05929984fd10566881caade5ca377e18ebd666c4
son-n-pham <21343675@student.uwa.edu.au>
2021-06-25 00:39:12 +0800

    Day 18 not completed

---
+++ day18_turtle/my_turtle.py
@@ -0,0 +1,91 @@
+from turtle import Turtle, Screen
+import turtle
+import random
+
+
+# Practice not only turtle but class and class inheritance
+class MyTurtle(Turtle):
+    def __init__(self):
+        Turtle.__init__(self)
+        self.shape("turtle")
+
+    def DrawSquare(self, side=100, angle=90):
+        for i in range(4):
+            self.forward(side)
+            self.right(angle)
+
+    def DrawDashLine(self, dashLineLength=500, lineLength=5, spaceLength=2):
+        distance = 0
+        while distance < dashLineLength:
+            self.forward(lineLength)
+            self.penup()
+            self.forward(spaceLength)
+            self.pendown()
+            distance += lineLength + spaceLength
+
+    def DrawRandomShape(self, side=100, initNumberOfSide=3):
+        turtleColor = ["red", "orange", "blue", "purple", "yellow", "black", "green"]
+        numberOfSide = initNumberOfSide
+        while True:
+            self.pencolor(random.choice(turtleColor))
+            for i in range (numberOfSide):
+                self.forward(side)
+                self.right(360/numberOfSide)
+            numberOfSide += 1
+
+    def RandomColor(self):
+        turtle.colormode(255)
+        self.pencolor((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+
+    def RandomWalk(self, length=20):
+        #Comment out this list of color as the colormode is more advance.
+        #turtleColor = ["red", "orange", "blue", "purple", "yellow", "black", "green"]
+
+        #To use the color randomly, we use colormode of turtle class to set it to 255,
+        #then we can use the random.randint in the while loop
+        #turtle.colormode(255)
+
+        direction = [0, 90, 180, 270]
+        self.pensize(10)
+        self.speed("fast")
+        self.hideturtle()
+        while True:
+            #self.pencolor(random.choice(turtleColor))
+            #self.pencolor((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+
+            self.RandomColor()
+            turtleHeadingAngle = self.setheading(random.choice(direction))
+            self.forward(length)
+
+    def spiroGraph(self, angle=10):
+        currentAngle=0
+        while True:
+            self.speed("fastest")
+            self.setheading(currentAngle)
+            self.RandomColor()
+            self.circle(100)
+            currentAngle += angle
+            if currentAngle%360==0:
+                break
+        
+            
+
+tim = MyTurtle()
+
+#Challenge 1: Draw Square
+#tim.DrawSquare()
+
+#Challenge 2: Draw Dash Line
+#tim.DrawDashLine()
+
+#Challenge 3: Draw different shapes with increasing numbers of sides and random colors
+#tim.DrawRandomShape()
+
+#Challenge 4: Random walk
+#tim.RandomWalk()
+
+#Challenge 5: Draw a Spirograph
+tim.spiroGraph(13)
+
+screen = Screen()
+screen.exitonclick()
